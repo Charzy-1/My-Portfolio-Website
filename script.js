f7bf5bf43e357952ce9f437c1803b6615def48fc
Charles <chazylites@gmail.com>
2024-06-18 19:26:52 +0300

    i created a function on javascript to open-popupup and an id was passed as an argument to make the popup work

--- script.js
+++ script.js
@@ -87,24 +87,3 @@ document.getElementById('close-btn').onclick = function() {
 };
 
 
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const seeProjectButton = document.getElementById('see-project');
-//   const popup = document.getElementById('popup');
-//   const closePopupButton = document.getElementById('close-popup');
-
-//   seeProjectButton.addEventListener('click', () => {
-//     popup.classList.add('active');
-//   });
-
-//   closePopupButton.addEventListener('click', () => {
-//     popup.classList.remove('active');
-//   });
-
-//   // Close the popup if the user clicks outside of it
-//   window.addEventListener('click', (event) => {
-//     if (event.target === popup) {
-//       popup.classList.remove('active');
-//     }
-//   });
-// });
