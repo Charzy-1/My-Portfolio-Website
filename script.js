0dd88cee938c5d32f5d3f5f9b40ead473a32562f
Charles <chazylites@gmail.com>
2024-06-19 19:44:06 +0300

    I updated the description of popup cards dynamically

--- script.js
+++ script.js
@@ -15,64 +15,82 @@ document.querySelectorAll('.navLink').forEach(n => n.addEventListener('click', (
 const projects = [
   {
     title: 'Multi-Post Services',
+    longTitle: 'Keeping tracks of hundreds of components websites',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-    imgSrc: './images/Img Placeholder (1).png',
-    liveLink: '#',
-    sourceLink: '#'
-  },
-  {
-    title: 'Multi-Post Services',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.\n\n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
     imgSrc: './images/Snapshoot Portfolio.png',
     liveLink: '#',
     sourceLink: '#'
   },
   {
     title: 'Multi-Post Services',
+    longTitle: 'Keeping tracks of hundreds of components websites',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.\n\n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
     imgSrc: './images/Snapshoot Portfolio.png',
     liveLink: '#',
     sourceLink: '#'
   },
   {
     title: 'Multi-Post Services',
+    longTitle: 'Keeping tracks of hundreds of components websites',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.\n\n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
     imgSrc: './images/Snapshoot Portfolio.png',
     liveLink: '#',
     sourceLink: '#'
   },
   {
     title: 'Multi-Post Services',
+    longTitle: 'Keeping tracks of hundreds of components websites',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.\n\n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
     imgSrc: './images/Snapshoot Portfolio.png',
     liveLink: '#',
     sourceLink: '#'
   },
   {
     title: 'Multi-Post Services',
+    longTitle: 'Keeping tracks of hundreds of components websites',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.\n\n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
     imgSrc: './images/Snapshoot Portfolio.png',
     liveLink: '#',
     sourceLink: '#'
   },
   {
     title: 'Multi-Post Services',
+    longTitle: 'Keeping tracks of hundreds of components websites',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.\n\n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
     imgSrc: './images/Snapshoot Portfolio.png',
     liveLink: '#',
     sourceLink: '#'
-  }
+  },
+  {
+    title: 'Multi-Post Services',
+    longTitle: 'Keeping tracks of hundreds of components websites',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.\n\n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    imgSrc: './images/Snapshoot Portfolio.png',
+    liveLink: '#',
+    sourceLink: '#'
+  },
 ];
 
 function open_popup(id) {
   const project = projects[id - 0]; // id is 1-based index, so adjust to 0-based
-  document.getElementById('popup-title').innerText = project.title;
-  document.getElementById('popup-description').innerText = project.description;
+  const isWideScreen = window.matchMedia("(min-width: 820px)").matches;
+
+  document.getElementById('popup-title').innerText = isWideScreen ? project.longTitle : project.title;
+  document.getElementById('popup-description').innerText = isWideScreen ? project.longDescription : project.description;
+
   document.getElementById('popup-image').src = project.imgSrc;
   document.getElementById('popup-live-link').href = project.liveLink;
   document.getElementById('popup-source-link').href = project.sourceLink;
   document.getElementById('modal-popup').style.display = 'block';  
 }
+
 //At the click of the window, the popup closes
 let modal = document.getElementById('modal-popup');
 window.onclick = function(event) {
